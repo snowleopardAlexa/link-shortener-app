8a0ee964bcfb5074460657669b453e5c23d6ce03
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-28 21:55:37 -0500

    connect to db, test with postman

--- pages/api/shorten_link.tsx
+++ pages/api/shorten_link.tsx
@@ -2,16 +2,28 @@
 import { MongoClient } from 'mongodb';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+let cachedDb;
 
+async function connectToDatabase() {
+  if (cachedDb) {
+    return cachedDb;
+  }
+  const client = new MongoClient(process.env.MONGODB_URI, { useNewUrlParser: true });
 
-  if (req.body.length > 0) {
-    res.statusCode = 201;
-    return res.json({ short_link: 'hello'});
+  cachedDb = client;
+  return await client.connect();
+}
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const db = await connectToDatabase();
+
+  if (req.body.link) {
+    const entry = await db.db('links_db').collection('links_collection').insertOne({ link: req.body.link });
+
+    res.statusCode = 201; // created
+    return res.json({ short_link: `${process.env.VERCEL_URL}/r/${entry.insertedId}` });
   }
 
-  res.statusCode = 409;
-  res.json({ error: 'no_link_found', error_description: 'No link found.'})
-
-  res.status(200).json({ name: "John Doe" });
+  res.statusCode = 409; // conflict
+  res.json({ error: 'no_link_found', error_description: 'No link found'})
 }
